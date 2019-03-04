bdb31f85320fd34384b7348fa7a3a2fa86d72c3a
Owain Kenway <o.kenway@ucl.ac.uk>
2019-03-04 12:24:00 +0000

    Templates for Grace at UCL.

---
+++ cmake/toolchain.grace.gcc.cmake
@@ -0,0 +1,21 @@
+# Compilers
+set(CMAKE_C_COMPILER       /shared/ucl/apps/gcc/4.9.2/bin/gcc)
+set(CMAKE_CXX_COMPILER     /shared/ucl/apps/gcc/4.9.2/bin/g++)
+set(CMAKE_Fortran_COMPILER /shared/ucl/apps/gcc/4.9.2/bin/gfortran)
+
+# Hardware Specification
+set(DIRAC3_HOST          grace)
+set(HOST_CPUS_PER_NODE       16)
+set(HOST_THREADS_PER_CPU      1)
+set(HOST_COMPILER_ID        gcc)
+set(HOST_SUBMIT_COMMAND    qsub)
+
+# Speciy the path to the FFTW2 library manually instead
+# of using the module due to a conflict error on Archer
+set(FFTW2_ROOT /shared/ucl/apps/fftw/2.1.5/gnu-4.9.2)
+
+# Find MPI library but use aprun for parallel jobs
+find_program(MPIEXEC gerun)
+find_package(MPI REQUIRED)
+set(MPIEXEC_NUMPROC_FLAG -np)
+set(MPIEXEC_PREFLAGS "")
